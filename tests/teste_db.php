1c566c1dc68e118759f1ec3ee4686e8dfe513391
Pedro Novaes <138346245+PedroNov4es@users.noreply.github.com>
2025-11-01 20:44:43 -0300

    Comentário adicionado

--- tests/teste_db.php
+++ tests/teste_db.php
@@ -1,4 +1,6 @@
 <?php
+//Verifica se a conexão com o banco de dados está funcionando corretamente.
+
 include "db.php";
-echo "✅ Conexão bem-sucedida com o banco de dados!";
+echo "✅ Conexão bem-sucedida com o banco de dados!"; //imprime na tela
 ?>
